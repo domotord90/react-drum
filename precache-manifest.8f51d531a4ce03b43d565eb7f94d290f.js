eaef94fd65f28013b32ba3313980bc1526049867
domotord90 <jazzmaster@freemail.com>
2019-11-16 19:11:43 +0100

    Updates

---
+++ precache-manifest.8f51d531a4ce03b43d565eb7f94d290f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088b1c776a102af79d59060ec071646b",
+    "url": "/react-drum/index.html"
+  },
+  {
+    "revision": "00620adc2aa8c7ed200f",
+    "url": "/react-drum/static/css/main.3608ba41.chunk.css"
+  },
+  {
+    "revision": "8a12b9fc33f79e1ac7ce",
+    "url": "/react-drum/static/js/2.01e1a794.chunk.js"
+  },
+  {
+    "revision": "00620adc2aa8c7ed200f",
+    "url": "/react-drum/static/js/main.da7faa86.chunk.js"
+  },
+  {
+    "revision": "74330b7ba3a05808dec0",
+    "url": "/react-drum/static/js/runtime~main.14d1d5cf.js"
+  },
+  {
+    "revision": "ee9072cfc9314d18a8e614104a805ab1",
+    "url": "/react-drum/static/media/DIGITALDREAM.ee9072cf.ttf"
+  }
+]);
